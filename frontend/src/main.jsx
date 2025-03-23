73b4e9d5ea6652dbdbac5b67f78726fd7fc9c1fb
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-03-23 09:55:47 +0700

    fix(components): update sidenavlist to use navlink instead of button

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,10 @@ import LogInPage from "./pages/LogInPage.jsx";
 import SignUpPage from "./pages/SignUp/SignUpPage.jsx";
 import HomePage from "./pages/Home/HomePage.jsx";
 import DashboardPage from "./pages/Dashboard/DashboardPage.jsx";
+import AddFoodPage from "./pages/AddFood/AddFoodPage.jsx";
+import FoodDatabasePage from "./pages/FoodDatabase/FoodDatabasePage.jsx";
+import UserAccountPage from "./pages/UserAccount/UserAccountPage.jsx";
+import DashboardLayout from "./components/DashboardLayout";
 
 const router = createBrowserRouter([
   {
@@ -21,10 +25,28 @@ const router = createBrowserRouter([
     element: <SignUpPage />,
   },
   {
-    path: "/dashboard",
-    element: <DashboardPage />,
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "/addfood",
+        element: <AddFoodPage />,
+      },
+      {
+        path: "/database",
+        element: <FoodDatabasePage />,
+      },
+      {
+        path: "/profile",
+        element: <UserAccountPage />,
+      },
+    ],
   },
 ]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
