c5e97c79ba2b24cc20b31c3b46716892ca81865a
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-03-23 14:42:28 +0700

    feat(providers): add screensize useEffect for mobile UX (collapsed by default in sidenav)

--- frontend/src/providers/SidebarContext.jsx
+++ frontend/src/providers/SidebarContext.jsx
@@ -1,9 +1,19 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const SidebarContext = createContext();
 
 export const SidebarProvider = ({ children }) => {
-  const [expanded, setExpanded] = useState(true);
+  // lg breakpoint in tailwind
+  const [expanded, setExpanded] = useState(() => window.innerWidth >= 1024);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setExpanded(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <SidebarContext.Provider value={{ expanded, setExpanded }}>
