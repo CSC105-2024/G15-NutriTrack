5870e052e7d5e30faaafb5b2de8d7ede95117da2
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-03-23 09:57:02 +0700

    feat(sidebar)!: update sidenavlink usages

--- frontend/src/pages/Dashboard/components/DashboardSidebar.jsx
+++ frontend/src/pages/Dashboard/components/DashboardSidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   faChevronLeft,
   faChevronRight,
@@ -12,89 +12,51 @@ import { useSidebar } from "../../../providers/SidebarContext";
 import SideNavList from "./../../../components/SideNavList";
 import ThemeToggle from "../../../components/ThemeToggle";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useNavigate } from "react-router-dom";
 
 const DashboardSidebar = () => {
-  const navigate = useNavigate();
-  const [active, setActive] = useState("Home");
   const { expanded, setExpanded } = useSidebar();
 
   return (
-    <aside className="absolute top-0 left-0 h-screen z-10">
-      <nav
-        className={`h-full flex flex-col bg-white border-r shadow-sm transition-all ${
-          expanded ? "w-56" : "w-16"
-        }`}
-      >
+    <aside
+      className={`h-screen bg-white border-r shadow-sm transition-all duration-300 ${
+        expanded ? "w-56" : "w-16"
+      }`}
+    >
+      <nav className="h-full flex flex-col">
         {/* Logo and toggle section */}
-        <div className="p-2 flex justify-between items-center mb-8 w-full">
-          <div className="flex items-center">
-            <img
-              src="/assets/logo/logo.png"
-              className={`rounded-full overflow-hidden transition-all ${
-                expanded ? "w-12 h-12" : "w-0 h-0"
-              }`}
-              alt="Logo"
-            />
-            {expanded && (
-              <span className="ml-4 text-lg text-green-700 font-medium">
-                NutriTrack
-              </span>
-            )}
-          </div>
-          <div className={`flex ${expanded ? "" : "w-full justify-center"}`}>
-            <button
-              onClick={() => setExpanded((prev) => !prev)}
-              className="p-1 rounded-full hover:bg-gray-100 cursor-pointer"
-            >
-              <FontAwesomeIcon
-                icon={expanded ? faChevronLeft : faChevronRight}
-                size="sm"
-              />
-            </button>
-          </div>
+        <div className="p-4 flex justify-between items-center">
+          <img
+            className={`rounded-full transition-all ${
+              expanded ? "w-12 h-12 sm:w-16 sm:h-16" : "w-8 h-8"
+            }`}
+            alt="NutriTrack logo"
+            src="/assets/logo/logo.png"
+          />
+          <button
+            onClick={() => setExpanded((prev) => !prev)}
+            className="p-2 rounded-full hover:bg-gray-100"
+          >
+            <FontAwesomeIcon icon={expanded ? faChevronLeft : faChevronRight} />
+          </button>
         </div>
 
-        <ul
-          className={`flex flex-col gap-4 ${expanded ? "p-2" : "p-4"} w-full`}
-        >
-          <SideNavList
-            text="Home"
-            icon={faHome}
-            onClick={() => setActive("Home")}
-            isActive={active === "Home"}
-          />
-          <SideNavList
-            text="Add Food"
-            icon={faPlus}
-            onClick={() => setActive("Add Food")}
-            isActive={active === "Add Food"}
-          />
-          <SideNavList
-            text="Database"
-            icon={faDatabase}
-            onClick={() => setActive("Database")}
-            isActive={active === "Database"}
-          />
-          <SideNavList
-            text="Profile"
-            icon={faUser}
-            onClick={() => setActive("Profile")}
-            isActive={active === "Profile"}
-          />
+        <ul className="flex flex-col gap-4 p-2 w-full">
+          <SideNavList text="Dashboard" icon={faHome} path="/dashboard" />
+          <SideNavList text="Add Food" icon={faPlus} path="/addfood" />
+          <SideNavList text="Database" icon={faDatabase} path="/database" />
+          <SideNavList text="Profile" icon={faUser} path="/profile" />
         </ul>
 
-        <div className="flex justify-center py-2">
+        <div className="flex-grow"></div>
+
+        <div className="flex justify-center p-4">
           <ThemeToggle />
         </div>
-
-        <div className="flex-grow"></div>
 
         <SideNavList
           text="Sign Out"
           icon={faSignOut}
-          onClick={() => navigate("/")}
-          isActive={active === "Sign Out"}
+          path="/"
           className="mb-16"
         />
       </nav>
