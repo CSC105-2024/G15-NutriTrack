e05d08c20eb7b5aa1637e48759240780a72d1cac
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-04-22 15:32:50 +0700

    feat(component): add pop up calendar component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         element: <AddFoodPage />,
       },
       {
-        path: "/database",
+        path: "/fooddatabase",
         element: <FoodDatabasePage />,
       },
       {
