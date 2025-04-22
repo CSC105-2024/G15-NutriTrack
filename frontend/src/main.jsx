6b7dac67bc004f787e0a98cb2f301b4419841853
Wai Yan Hein Oo <waiyanheinoo99@gmail.com>
2025-04-22 15:40:34 +0700

    feat(component): add pop up calendar component (#9)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -50,5 +50,5 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 );
