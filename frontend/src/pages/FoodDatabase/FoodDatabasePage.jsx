db7cffff6547771b1e79721680484f9893e42561
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-04-24 01:24:04 +0700

    page: implement food grid in database page

--- frontend/src/pages/FoodDatabase/FoodDatabasePage.jsx
+++ frontend/src/pages/FoodDatabase/FoodDatabasePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
+import FoodGrid from "./FoodGrid";
 
 const FoodDatabasePage = () => {
   useDocumentTitle("Food Database");
 
-  return <div>FoodDatabasePage</div>;
+  return (
+    <>
+      <div>FoodDatabasePage</div>
+      <FoodGrid />
+    </>
+  );
 };
 
 export default FoodDatabasePage;
