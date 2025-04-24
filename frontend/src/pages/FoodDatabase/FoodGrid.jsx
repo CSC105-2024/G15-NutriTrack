82898385c15ccf1ce611df46fe92486050b3acc4
Waiyanheinoo <waiyanheinoo99@gmail.com>
2025-04-24 15:14:25 +0700

    feat(component): implement search bar and change the layout from grid to flex

--- frontend/src/pages/FoodDatabase/FoodGrid.jsx
+++ frontend/src/pages/FoodDatabase/FoodGrid.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { foods } from "../../data/SampleData";
 import FoodDetailPopup from "../../components/FoodDetailPopup";
+import FormInput from "@/components/FormInput";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const FoodGrid = () => {
   const [selectedFood, setSelectedFood] = useState(null);
@@ -13,16 +15,33 @@ const FoodGrid = () => {
     setSelectedFood(null);
   };
 
+  const [searchItems, setSearchItems] = useState(foods);
+  const handleSearch = (e) => {
+    const searchValue = e.target.value.toLowerCase();
+    setSearchItems(
+      foods.filter((food) => food.name.toLowerCase().includes(searchValue))
+    );
+  };
+
   return (
-    <div className="p-6">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-        {foods.map((food, index) => (
+    <div className="py-6">
+      <div className="mb-8 w-80 lg:w-120 mx-auto sticky top-0 bg-white z-10 rounded-2xl">
+        <FormInput
+          type={"text"}
+          placeholder={"Search"}
+          onChange={(e) => handleSearch(e)}
+          iconFa={faMagnifyingGlass}
+        />
+      </div>
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6"> */}
+      <div className="flex flex-wrap items-center justify-center gap-6">
+        {searchItems.map((food, index) => (
           <div
             key={index}
-            className="bg-white border border-black rounded-xl p-4 cursor-pointer"
+            className="w-50 bg-white border border-black rounded-xl p-4 cursor-pointer"
             onClick={() => handleCardClick(food)}
           >
-            <div className="bg-[#e9f5e8] min-h-56 rounded-lg p-4 flex flex-col items-center hover:bg-[#e8fcd4] transition-colors justify-between">
+            <div className=" bg-[#e9f5e8] min-h-56 rounded-lg p-4 flex flex-col items-center hover:bg-[#e8fcd4] transition-colors justify-between">
               <img
                 src={food.imgUrl}
                 alt={food.name}
