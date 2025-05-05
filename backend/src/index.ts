82da1dcea624278f3febbe52ca4cc915a1849092
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-06 00:24:35 +0700

    feat(pkgs): implement cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { mainRouter } from "./routes/index.route.ts";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+app.use("*", cors());
 
 app.route("", mainRouter);
 
