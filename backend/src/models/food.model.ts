c90c74269c57c85f52493c76bd187ec9bdd8d773
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-05-06 14:53:17 +0700

    update food model to not select

--- backend/src/models/food.model.ts
+++ backend/src/models/food.model.ts
@@ -1,17 +1,7 @@
 import db from "../lib/db.ts";
 
 const getAllFoods = async () => {
-  const foods = await db.defaultFood.findMany({
-    select: {
-      id: true,
-      name: true,
-      calories: true,
-      protein: true,
-      fat: true,
-      carb: true,
-      mealType: true,
-    },
-  });
+  const foods = await db.defaultFood.findMany();
   return foods;
 };
 
